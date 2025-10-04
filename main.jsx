07c2002f1b227779d60211a4c27adee27187c359
Ana Zubieta Pena <azubieta@c2r9s3.42malaga.com>
2025-10-04 13:41:00 +0200

    pagina explicativa proyecto mars_recycling_system para la NASA

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MarsRecyclingSystem from "./MarsRecyclingSystem";
+import "./index.css"; // Tailwind CSS
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MarsRecyclingSystem />
+  </React.StrictMode>
+);
